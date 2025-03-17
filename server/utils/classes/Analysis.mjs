5e6188f6ab046fbc08d6f517decf126bc7eccab9
Sergio Navarro <s.navarroredondo@gmail.com>
2025-03-17 21:53:32 +0100

    ADD Analysis class

---
+++ server/utils/classes/Analysis.mjs
@@ -0,0 +1,10 @@
+export class Analysis {
+  constructor(reqBody, analysisOwner) {
+    this.analysis_name = reqBody.analysisName;
+    this.analysis_url = reqBody.analysisUrl;
+    this.analysis_status = 'in_progress';
+    this.analysis_owner_id = analysisOwner;
+    this.analysis_tasks = reqBody.analysisTasks;
+    this.max_number_of_participants = reqBody.maxNumberOfParticipants;
+  }
+}
