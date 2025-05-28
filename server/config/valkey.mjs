6e8df7a07badb2132f7e5d0b31e61e516a7a7fc4
Sergio Navarro <s.navarroredondo@gmail.com>
2025-05-28 10:33:42 +0200

    ADD env variables to valkey config

--- server/config/valkey.mjs
+++ server/config/valkey.mjs
@@ -2,15 +2,19 @@ import { GlideClient, GlideClusterClient, Logger } from '@valkey/valkey-glide';
 import { logError } from './loggerFunctions.mjs';
 // When Valkey is in standalone mode,
 // add address of the primary node, and any replicas you'd like to be able to read from.
-const addresses = [
-  {
-    host: 'uxmonkeys-cache',
-    port: 6379,
-  },
-];
+
+const valkeyOptions = {
+  addresses: [
+    {
+      host: process.env.VALKEY_CACHE_HOST,
+      port: process.env.VALKEY_CACHE_PORT,
+    },
+  ],
+  clientName: 'uxmonkeys-cache',
+};
 
 // Check `GlideClientConfiguration/GlideClusterClientConfiguration` for additional options.
-export const valkeyClient = await GlideClient.createClient({
+export const valkeyClient = await GlideClient.createClient(valkeyOptions);
   addresses: addresses,
   // if the server uses TLS, you'll need to enable it. Otherwise,
   // the connection attempt will time out silently.
