d9a549e4d89c7920cf66a0fb892f351eafe4e414
Sergio Navarro <s.navarroredondo@gmail.com>
2025-06-10 00:31:35 +0200

    REFACTOR Analysis class to require analysis device from req.body

--- server/utils/classes/Analysis.mjs
+++ server/utils/classes/Analysis.mjs
@@ -2,7 +2,7 @@ export class Analysis {
   constructor(reqBody, analysisOwner) {
     this.name = reqBody.name;
     this.url = reqBody.url;
-    this.device = 'computer'; //* Default until we offer additional devices
+    this.device = reqBody.device;
     this.status = 'published'; //* Default until we allow for drafts
     this.owner_id = analysisOwner;
     this.tasks = reqBody.tasks;
