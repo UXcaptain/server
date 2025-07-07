113d58d5279d278413a764d2ff1cfd82ac3d6ec2
Sergio N <s.navarroredondo@gmail.com>
2025-07-07 11:44:27 +0200

    Updates Telegram chat ID based on environment
    Updates the Telegram chat ID used for sending error
    logs based on the current environment.
    
    Uses a production ID in production and defaults to a
    test ID in non-production environments.

--- server/integrations/telegram/sendErrorLogsToTelegram.mjs
+++ server/integrations/telegram/sendErrorLogsToTelegram.mjs
@@ -1,7 +1,7 @@
 import { sendTelegramMessage } from '../../config/telegramBotConfig.mjs';
 
 export const sendErrorLogsToTelegram = async (error) => {
-  const chatId = process.env.TELEGRAM_BOT_LOGS_CHAT_ID; // Flightsnatch error logs group
+  const chatId = process.NODE_ENV === 'production' ? 'tbd' : '-1002730767538';
   const message = `
   🚨 <b>New error logged at ${new Date()}</b> 🚨
   \n<b>Error Name</b>: ${error.name}
