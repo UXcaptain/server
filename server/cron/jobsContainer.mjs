56a5675bdea31ca936c9f94c6ccaa99a31455934
Sergio N <s.navarroredondo@gmail.com>
2025-07-30 22:32:12 +0200

    Adds new cron job to mark analysis entries as cancelled daily.
    - Introduces new scheduler for marking expired analysis entries every hour
    - Ensures that any errors during the process are logged appropriately.

--- server/cron/jobsContainer.mjs
+++ server/cron/jobsContainer.mjs
@@ -1,9 +1,11 @@
 import { logError } from '../config/loggerFunctions.mjs';
 import { deletePasswordResetTokensScheduler } from './deletePasswordResetTokensScheduler.mjs';
+import { markAnalysisEntriesAsCancelledScheduler } from './markAsCancelledAnalysisEntriesScheduler.mjs';
 
 export const startCronJobs = () => {
   try {
     deletePasswordResetTokensScheduler.start();
+    markAnalysisEntriesAsCancelledScheduler.start();
 
     console.log('Cron jobs started');
   } catch (error) {
