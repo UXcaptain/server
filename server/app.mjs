c53c8bf2a7c16b9ec8560cac9eb16256da3e425f
Sergio N <s.navarroredondo@gmail.com>
2025-10-04 14:07:12 +0200

    Revert "REMOVE rate & slow limiter for testing in self hosted environment"
    This reverts commit 54da6d4e5132b825d292ed6ecb8de3966f8c35ff.
    
    The error was in the compose.yaml file which has not been updated in the server

--- server/app.mjs
+++ server/app.mjs
@@ -20,8 +20,8 @@ const server = createServer(app);
 app.use(helmetMiddleware);
 app.use(corsMiddleware);
 app.use(cookieParserMiddleware);
-// if (process.env.NODE_ENV === 'production') app.use(slowLimiter);
-// if (process.env.NODE_ENV === 'production') app.use(limiter);
+if (process.env.NODE_ENV === 'production') app.use(slowLimiter);
+if (process.env.NODE_ENV === 'production') app.use(limiter);
 
 //* Webhooks router
 app.use('/webhooks', webhookRouter);
