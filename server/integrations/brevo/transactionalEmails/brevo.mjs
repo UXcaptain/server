fae2b3e8784bd7985dae13b62eda1d1ed9fc2223
Sergio N <s.navarroredondo@gmail.com>
2025-10-12 23:38:25 +0200

    cleanup syntax in brevo-related code

--- server/integrations/brevo/transactionalEmails/brevo.mjs
+++ server/integrations/brevo/transactionalEmails/brevo.mjs
@@ -7,8 +7,8 @@ apiKey.apiKey = process.env.BREVO_API_KEY;
 export const companyEmails = {
   support: 'suport@uxcaptain.com',
   billing: 'billing@uxcaptain.com',
-  hello: 'hello@uxcaptain.com'
-}
+  hello: 'hello@uxcaptain.com',
+};
 
 export const emailInstance = new brevo.SendSmtpEmail();
 
