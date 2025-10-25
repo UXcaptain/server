6ff744eeb60475d964290c5a7f27bc4a8c5cd98a
Sergio N <s.navarroredondo@gmail.com>
2025-10-25 21:39:47 +0200

    REFACTOR aws_region usage into a ENV variable

--- server/integrations/aws/s3.js
+++ server/integrations/aws/s3.js
@@ -3,7 +3,7 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-s3/modules/credentials.html
 export const s3client = new S3Client({
-  region: 'eu-west-3',
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
