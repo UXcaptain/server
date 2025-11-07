9af3b46798c05370b32cb926cd81745883682ab8
Sergio N <s.navarroredondo@gmail.com>
2025-11-07 15:45:41 +0100

    WIP in handleTranscriptionCompletedQueue
    Indicate next steps

--- server/config/messageBroker/transcriptionCompletedQueue.js
+++ server/config/messageBroker/transcriptionCompletedQueue.js
@@ -2,7 +2,5 @@ export const handleTranscriptionCompletedQueue = async (msg) => {
   const contentStr = msg.bodyToString();
   const content = JSON.parse(contentStr);
 
-  // TODO - process handleTranscriptionCompleted(content);
-
-  return;
+  // TODO - store the completed JSON that will be provided by the transcription microservice and return it to the front end it available -- Need to update DB schema to include the field
 };
