6a7666d2bba446df9ee224514cb083132f777c32
Sergio N <s.navarroredondo@gmail.com>
2025-12-21 20:17:18 +0100

    ADD error logging to completedTranscriptionJobScheduler CRON job

--- server/cron/getCompletedTranscriptionJobsScheduler.js
+++ server/cron/getCompletedTranscriptionJobsScheduler.js
@@ -1,10 +1,11 @@
 import { CronJob } from 'cron';
 import { handleCompletedVideoTranscriptionJobs } from '../services/analysisService.js';
+import { logError } from '../config/loggerFunctions.js';
 
 export const getCompletedTranscriptionJobsScheduler = new CronJob('15 * * * *', async () => {
   try {
     await handleCompletedVideoTranscriptionJobs();
   } catch (error) {
-    console.error('Error checking transcription job status:', error);
+    logError('Error checking transcription job status', error);
   }
 });
