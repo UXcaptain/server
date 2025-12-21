fb3ce07e9df9f0b8facf64a63a71674ec203ceb2
Sergio N <s.navarroredondo@gmail.com>
2025-12-21 20:16:41 +0100

    REMOVE lavinMQ implementation

--- server/app.js
+++ server/app.js
@@ -12,7 +12,6 @@ import { slowLimiter } from './middlewares/express-slow-down.js';
 import { startCronJobs } from './cron/jobsContainer.js';
 import { globalErrorHandler } from './middlewares/globalErrorHandler.js';
 import { webhookRouter } from './webhooks/webhooksRouter.js';
-import { connectToMessageBroker } from './config/messageBroker/LavinMQ.js';
 
 const app = express();
 const server = createServer(app);
@@ -50,7 +49,6 @@ server.listen(process.env.PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT}/`);
 });
 
-connectToMessageBroker();
 startCronJobs();
 
 const gracefulShutdown = () => {
