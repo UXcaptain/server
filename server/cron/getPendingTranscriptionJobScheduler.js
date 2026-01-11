483078de25f3b25a7e26d4fa0125a14b33f269ac
Sergio N <s.navarroredondo@gmail.com>
2026-01-11 21:46:45 +0100

    UPDATE processing transcriptions to 5 minutes

--- server/cron/getPendingTranscriptionJobScheduler.js
+++ server/cron/getPendingTranscriptionJobScheduler.js
@@ -1,6 +1,6 @@
 import { CronJob } from 'cron';
 import { processPendingTranscriptionJobs } from '../controllers/transcriptionController.js';
 
-export const getPendingTranscriptionJobScheduler = new CronJob('* * * * *', async () => {
+export const getPendingTranscriptionJobScheduler = new CronJob('*/5 * * * *', async () => {
   processPendingTranscriptionJobs();
 });
