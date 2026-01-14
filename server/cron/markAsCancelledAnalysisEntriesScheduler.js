9b53be677a803629eee0398fa806c9f6e42fd61b
Sergio N <s.navarroredondo@gmail.com>
2026-01-14 22:27:35 +0100

    REFACTOR expired analysisEntries to fire-and-forget function

--- server/cron/markAsCancelledAnalysisEntriesScheduler.js
+++ server/cron/markAsCancelledAnalysisEntriesScheduler.js
@@ -1,14 +1,9 @@
 import { CronJob } from 'cron';
-import { logError } from '../config/loggerFunctions.js';
 import { markAnalysisEntriesAsCancelled } from '../models/analysisEntryModel.js';
 
 export const markAnalysisEntriesAsCancelledScheduler = new CronJob(
   '0 * * * *',
-  async () => {
-    try {
-      await markAnalysisEntriesAsCancelled();
-    } catch (error) {
-      logError('Error marking expired analysis entries as cancelled:', error);
-    }
+  () => {
+    markAnalysisEntriesAsCancelled();
   },
 );
